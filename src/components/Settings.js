8e0148807952851ec9c76283e36cab25eb575c38
Leo J Alfred <leojalfred@gmail.com>
2020-06-26 10:46:35 -0500

    Renders settings dropdown on settings click

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import classNames from 'classnames'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCog } from '@fortawesome/free-solid-svg-icons'
+import './Settings.scss'
 
 export default function Settings() {
-  return <FontAwesomeIcon className="settings navbar__link" icon={faCog} />
+  const [dropdown, setDropdown] = useState()
+  const onClick = () => {
+    if (dropdown === null) setDropdown(<div>Cock</div>)
+    else setDropdown(null)
+  }
+
+  const active = dropdown !== null
+  const classes = classNames('settings navbar__link', {
+    'settings--active': active,
+  })
+  return (
+    <>
+      <FontAwesomeIcon className={classes} icon={faCog} onClick={onClick} />
+      {dropdown}
+    </>
+  )
 }
