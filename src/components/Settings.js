f3672296ed41c9f7fb5b92d0ca0b6c876de78b75
Leo J Alfred <leojalfred@gmail.com>
2020-06-26 10:29:17 -0500

    Fixes link margin styling

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCog } from '@fortawesome/free-solid-svg-icons'
 
 export default function Settings() {
-  return <div>cock</div>
+  return <FontAwesomeIcon className="settings navbar__link" icon={faCog} />
 }
