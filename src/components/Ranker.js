0cd18dda2b6d896117e5dd3123676202300064e2
Leo J Alfred <leojalfred@gmail.com>
2020-07-11 15:20:44 -0500

    Creates intial Ranker structure

--- src/components/Ranker.js
+++ src/components/Ranker.js
@@ -1,5 +1,33 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'
 
 export default function Ranker() {
-  return <div className="ranker">penis</div>
+  const [data, setData] = useState([
+    { id: '1', text: 'Minneapolis' },
+    { id: '2', text: 'Asheville' },
+  ])
+
+  const onDragEnd = useCallback(() => {}, [])
+
+  return (
+    <DragDropContext className="ranker" onDragEnd={onDragEnd}>
+      <Droppable droppableId="1">
+        {({ innerRef, droppableProps, placeholder }) => (
+          <div ref={innerRef}>
+            {data.map(({ id, text }, i) => (
+              <Draggable draggableId={id} key={id} index={i}>
+                {({ innerRef, draggableProps, dragHandleProps }) => (
+                  <div ref={innerRef} {...draggableProps} {...dragHandleProps}>
+                    {text}
+                  </div>
+                )}
+              </Draggable>
+            ))}
+
+            {placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  )
 }
