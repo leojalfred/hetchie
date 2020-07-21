343e65cf807298ac181280f4786682f6bcc61df0
Leo J Alfred <leojalfred@gmail.com>
2020-07-21 13:44:44 -0500

    Standardizes view container margin/padding

--- src/views/firms/Firms.js
+++ src/views/firms/Firms.js
@@ -2,5 +2,9 @@ import React from 'react'
 import './Firms.scss'
 
 export default function Firms() {
-  return <main className="firms container"></main>
+  return (
+    <main className="firms container">
+      <h1 className="firms__heading">Firms List</h1>
+    </main>
+  )
 }
