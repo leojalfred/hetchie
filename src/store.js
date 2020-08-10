8e3b5348c2e8dbf615b46a0d427b6fa918c63e01
Leo J Alfred <leojalfred@gmail.com>
2020-08-10 16:21:29 -0500

    Updates store to ensure application works when devtools not present

--- src/store.js
+++ src/store.js
@@ -3,13 +3,12 @@ import thunk from 'redux-thunk'
 import rootReducer from './reducers'
 
 const initialState = {}
-const middleware = [thunk]
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+let composed = compose(applyMiddleware(thunk))
+if (window.__REDUX_DEVTOOLS_EXTENSION__ !== undefined)
+  composed = compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-)
+
+const store = createStore(rootReducer, initialState, composed)
 export default store
