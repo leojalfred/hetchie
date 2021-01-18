3d1ac588a8024b81e94affba0eb109788135f366
Leo Alfred <leojalfred@gmail.com>
2021-01-17 21:49:42 -0600

    Works on import csv

--- src/views/Admin/Admin.js
+++ src/views/Admin/Admin.js
@@ -1,4 +1,6 @@
+import { useRef } from 'react'
 import axios from 'axios'
+import parse from 'csv-parse'
 import stringify from 'csv-stringify'
 import { saveAs } from 'file-saver'
 import './Admin.scss'
@@ -6,8 +8,42 @@ import Container from 'components/Container'
 import Button from 'components/Buttons/Button'
 
 export default function Admin() {
+  const input = useRef()
   function importFirms(event) {
     event.preventDefault()
+
+    const reader = new FileReader()
+    reader.onloadend = () => {
+      const options = { columns: ['_id', 'name', 'firmLink', 'chambersLink', 'vaultLink', 'locations', 'practices', 'requiredGPA', 'preferredGPA', 'largeSalary', 'smallSalary', 'rankings', 'qualifications', 'date'] }
+      parse(reader.result, options, (error, records) => {
+        if (!error) {
+          const data = records.map(record => ({
+            _id: record._id,
+            name: record.name,
+            links: {
+              firm: record.firmLink,
+              chambers: record.chambersLink,
+              vault: record.vaultLink
+            },
+            locations: record.locations,
+            practices: record.practices,
+            gpa: {
+              required: record.requiredGPA,
+              band: record.preferredGPA
+            },
+            salary: {
+              large: record.largeSalary,
+              small: record.smallSalary
+            },
+            rankings: record.rankings,
+            qualifications: record.qualifications,
+            date: record.date
+          }))
+          console.dir(data)
+        } else console.log(error)
+      })
+    }
+    reader.readAsText(input.current.files[0])
   }
 
   async function exportFirms() {
@@ -95,7 +131,7 @@ export default function Admin() {
           <div className="admin__group">
             <h2 className="admin__subheading">Import firm data</h2>
             <form onSubmit={importFirms}>
-              <input className="admin__file" type="file" accept=".csv" />
+              <input className="admin__file" type="file" accept=".csv" ref={input} />
               <div className="admin__radios">
                 <div className="admin__radio-line">
                   <input
